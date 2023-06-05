7ed7a0eb845d0488f0d179d0715acfba2d784481
Rebeka <Rebeka@BECCA>
2023-06-05 14:43:10 +0200

    Add project files.

---
+++ Nadomjesni otpor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace prvi
+{
+    class Program
+    {
+        /// <summary>
+        /// RAČUNANJE NADOMJESNOG OTPORA
+        /// </summary>
+        static void Main()
+        {
+            int r1;
+            int r2;
+            int r3;
+            double rUk;
+            Console.Write("\nUpišite vrijednost prvog otpornika: ");
+            r1 = int.Parse(Console.ReadLine());
+            Console.Write("\nUpišite vrijednot drugog otpornika: ");
+            r2 = int.Parse(Console.ReadLine());
+            Console.Write("\nUpišite vrijednot trećeg otpornika: ");
+            r3 = int.Parse(Console.ReadLine());
+            rUk = (r2 * r3) / (r2 + r3) + r1;
+            Console.WriteLine("Nadomjesni otpor otpornika iznosi: {0}", rUk);
+        }
+    }
+}
